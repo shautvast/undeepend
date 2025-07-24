decd49cf01ad34ce3dfe8e8b1bda4407f7b35962
Shautvast <shautvast@gmail.com>
2025-07-24 09:58:39 +0200

    added test for dependencyManagement

--- tests/maven/pom_parser_test.rs
+++ tests/maven/pom_parser_test.rs
@@ -11,23 +11,29 @@ fn test_pom_parser_is_correct() {
     assert_eq!(Some("jar".to_string()),pom.packaging);
     assert_eq!(Some("http://www.mockito.org".to_string()),pom.url);
 
-    assert_eq!(2, pom.properties.len());
+    assert_eq!(3, pom.properties.len());
     assert_eq!("17", pom.properties["maven.compiler.source"]);
     assert_eq!("21", pom.properties["maven.compiler.target"]);
+    assert_eq!("1.1", pom.properties["hamcrest.version"]);
 
     assert_eq!(2, pom.dependencies.len());
     let hamcrest = &pom.dependencies[0];
     assert_eq!("org.hamcrest", hamcrest.group_id);
     assert_eq!("hamcrest-core", hamcrest.artifact_id);
-    assert_eq!(Some("1.1".to_string()), hamcrest.version);
+    assert_eq!(None, hamcrest.version);
 
     let objenesis = &pom.dependencies[1];
     assert_eq!("org.objenesis", objenesis.group_id);
     assert_eq!("objenesis", objenesis.artifact_id);
     assert_eq!(Some("1.0".to_string()), objenesis.version);
-    assert!(pom.dependency_management.is_empty());
 
     assert_eq!(2, pom.modules.len());
     assert_eq!("a", pom.modules[0]);
     assert_eq!("b", pom.modules[1]);
+
+    assert_eq!(1, pom.dependency_management.len());
+    let hamcrest = &pom.dependency_management[0];
+    assert_eq!("org.hamcrest", hamcrest.group_id);
+    assert_eq!("hamcrest-core", hamcrest.artifact_id);
+    assert_eq!(Some("${hamcrest.version}".to_string()), hamcrest.version);
 }
